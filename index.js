1e8e751174dd1218e2ec4ae28fe4b192157fa2e2
Teewawa <dev.tiaram@gmail.com>
2022-04-20 19:40:04 -0700

    Modified index.js file - defined DOM elements - Header align left, dog name alighn left, image border radius to 50px, rotated images upside down, and changed color of the footer text to blue

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Select some elements...
+
+//Align the header to the left
+let header = document.querySelector("#page-header");
+header.style.textAlign = "left";
+
+//Iterate through all dogImages and set border radius to 50px
+//Flip the images so they are upside down
+let dogImages = document.querySelectorAll(".dog-image");
+for (let i = 0; i < dogImages.length; i++) {
+  dogImages[i].style.borderRadius = "50px";
+  dogImages[i].style.transform = "rotate(180deg)";
+}
+//Align dogs' name to left
+let dogNames = document.querySelectorAll(".dog-name");
+for (let i = 0; i < dogNames.length; i++) {
+  dogNames[i].style.textAlign = "left";
+}
+
+//Change color of footer text
+let footerText = document.querySelector(".footer");
+footerText.style.color = "blue";
+
+//Note variables here are defined as a DOM Element
